5c12a4d1d300cafa801091571eecabe696ca4345
KH <kahheng98q@gmail.com>
2022-09-13 11:13:45 +0800

    logger

--- src/controllers/TodosController.ts
+++ src/controllers/TodosController.ts
@@ -1,6 +1,6 @@
 import pool from "../config/dbconnector";
 import { CommonHelper } from "../helper/common.helper";
-
+import { logger } from "../logger/index";
 class TodosController {
   public async get(req: any, res: any) {
     try {
@@ -8,9 +8,10 @@ class TodosController {
       const sql = "SELECT * FROM users";
 
       const todos = await CommonHelper.common(sql);
-
+      logger.info(`${req.originalUrl} - ${req.method} - ${req.ip}`);
       res.send(todos);
     } catch (error) {
+      logger.error(error);
       res.status(400).send(error);
     }
   }
